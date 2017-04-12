88510db041d2f9227553e1580e9b4cfef187fa56
insou22 <zac.kologlu@gmail.com>
2017-04-12 15:42:24 +1000

    Update for CDI changes pt. 2

--- src/main/java/com/ulfric/prison/Prison.java
+++ src/main/java/com/ulfric/prison/Prison.java
@@ -1,7 +1,6 @@
 package com.ulfric.prison;
 
 import com.ulfric.commons.spigot.plugin.UlfricPlugin;
-import com.ulfric.dragoon.container.Container;
 import com.ulfric.prison.command.ContainerCommands;
 import com.ulfric.prison.perk.ContainerPerks;
 
@@ -10,10 +9,8 @@ public final class Prison extends UlfricPlugin {
 	@Override
 	public void init()
 	{
-		Container container = this.getContainer();
-
-		container.install(ContainerPerks.class);
-		container.install(ContainerCommands.class);
+		this.install(ContainerPerks.class);
+		this.install(ContainerCommands.class);
 	}
 
 }
