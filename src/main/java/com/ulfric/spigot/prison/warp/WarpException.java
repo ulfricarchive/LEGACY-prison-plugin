3e8c6dae279fa5a75e77ef8153bbd064c4696e0a
Luke <development@inkzzz.com>
2017-04-18 01:26:18 +0100

    whoops - left overlay

--- src/main/java/com/ulfric/spigot/prison/warp/WarpException.java
+++ src/main/java/com/ulfric/spigot/prison/warp/WarpException.java
@@ -1,9 +1,5 @@
 package com.ulfric.spigot.prison.warp;
 
-/******************************************************************************
- * Copyright (c) 2017.  Written by Luke, associated with "Studio Ink LTD." *
- * Date: 18/04/2017                                                              *
- ******************************************************************************/
 public class WarpException extends RuntimeException {
 
     protected WarpException(String s, Exception e)
