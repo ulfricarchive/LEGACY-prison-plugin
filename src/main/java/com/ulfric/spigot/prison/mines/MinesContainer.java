b0586119ed19b7b40ca9274d478860e7b4238b65
Luke <development@inkzzz.com>
2017-05-03 00:13:30 +0100

    removed un needed code

--- src/main/java/com/ulfric/spigot/prison/mines/MinesContainer.java
+++ src/main/java/com/ulfric/spigot/prison/mines/MinesContainer.java
@@ -1,35 +1,14 @@
 package com.ulfric.spigot.prison.mines;
 
-import com.ulfric.commons.spigot.service.ServiceUtils;
-import com.ulfric.dragoon.ObjectFactory;
 import com.ulfric.dragoon.container.Container;
 import com.ulfric.dragoon.initialize.Initialize;
-import com.ulfric.dragoon.inject.Inject;
 
 public class MinesContainer extends Container {
-
-	@Inject
-	private ObjectFactory factory;
-
-	private MinesService mines;
 
 	@Initialize
 	private void initialize()
 	{
-		this.factory.bind(Mines.class).to(MinesService.class);
 		this.install(MinesService.class);
-	}
-
-	@Override
-	public void onEnable()
-	{
-		this.mines = ServiceUtils.getService(MinesService.class);
-	}
-
-	@Override
-	public void onDisable()
-	{
-		ServiceUtils.unregister(MinesService.class, this.mines);
 	}
 
 }
